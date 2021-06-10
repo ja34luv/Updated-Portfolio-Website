06830d7da89ccedae55546898b524c3166947deb
Jay <ja34luv@gmail.com>
2021-06-10 14:41:45 -0700

    Add existing file

---
+++ backup circle.js
@@ -0,0 +1,67 @@
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-size: 16px;
+}
+
+#landing_page {
+  height: 100vh;
+}
+
+#gallery {
+  position: relative;
+  width: 60rem;
+  height: 60rem;
+  padding: 10.16px;
+  border: solid 5px black;
+  border-radius: 50%;
+  margin: auto;
+}
+
+#gallery .gallery_item {
+  display: inline-block;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin: -146.165px 0 0 -103.33px;
+  width: 206.66px; /* A4 in pixels is 2480 x 3508 px divide this by 12 */
+  height: 292.33px;
+  background-color: gray;
+}
+
+#gallery .gallery_item img {
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+
+.deg0 {
+  transform: translate(30rem);
+} /* 30rem = half the width of the wrapper */
+
+.deg45 {
+  transform: rotate(45deg) translate(30rem) rotate(-45deg);
+}
+
+.deg90 {
+  transform: rotate(90deg) translate(30rem) rotate(-90deg);
+}
+
+.deg135 {
+  transform: rotate(135deg) translate(30rem) rotate(-135deg);
+}
+.deg180 {
+  transform: translate(-30rem);
+}
+.deg225 {
+  transform: rotate(225deg) translate(30rem) rotate(-225deg);
+}
+
+.deg270 {
+  transform: rotate(270deg) translate(30rem) rotate(-270deg);
+}
+
+.deg315 {
+  transform: rotate(315deg) translate(30rem) rotate(-315deg);
+}
---
+++ javaScript.js
@@ -0,0 +1,26 @@
+'use strict';
+
+startRotation();
+function startRotation() {
+  var rotateElement = document.getElementsByClassName('rotate-this');
+  new Propeller(rotateElement, {
+    inertia: 0.8,
+    speed: 10,
+  });
+}
+
+function is_touch_device() {
+  var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
+  var mq = function (query) {
+    return window.matchMedia(query).matches;
+  };
+
+  if (
+    'ontouchstart' in window ||
+    (window.DocumentTouch && document instanceof DocumentTouch)
+  ) {
+    return true;
+  }
+  var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
+  return mq(query);
+}
